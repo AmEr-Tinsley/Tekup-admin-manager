a8077a793e22a4ec4ee079200023512bf9e8e894
AmEr-Tinsley <amerhosni07@gmail.com>
2020-08-28 16:45:36 +0100

    deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const admin = require('./routes/admin');
 app.use('/admin', admin);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('frontend/build'))
+  app.use(express.static('frontend/dist'))
 
   app.get('*',(req,res) => {
      res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
