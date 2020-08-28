b7dce5b3511336a846252a1b929bef65bd433d89
AmEr-Tinsley <amerhosni07@gmail.com>
2020-08-28 16:38:45 +0100

    deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/build'))
 
   app.get('*',(req,res) => {
-     res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+     res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
   });
 }
 const PORT = process.env.PORT || 5000
