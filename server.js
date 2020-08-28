b07a0d6af9fa607c22435f1496e4bdd791a5974b
AmEr-Tinsley <amerhosni07@gmail.com>
2020-08-28 17:22:58 +0100

    deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/admin', admin);
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/dist'))
 
-  app.get('*',(req,res) => {
+  app.get('/.*/',(req,res) => {
      res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
   });
 }
